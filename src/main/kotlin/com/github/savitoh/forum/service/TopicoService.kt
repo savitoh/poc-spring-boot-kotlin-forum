ed963e3147f9e92e1cc0e51235b056a77b59cb36
Sávio Raires <savioraires10@gmail.com>
2021-07-25 03:47:17 -0300

    Adicionando mais livros

--- src/main/kotlin/com/github/savitoh/forum/service/TopicoService.kt
+++ src/main/kotlin/com/github/savitoh/forum/service/TopicoService.kt
@@ -6,23 +6,49 @@ import com.github.savitoh.forum.modelo.Usuario
 import org.springframework.stereotype.Service
 
 @Service
-class TopicoService {
+class TopicoService(private var topicos: List<Topico>) {
 
-    private val topico = Topico(
-        id = 1,
-        titulo = "Como tornar código Kotlin mais indiomático?",
-        mensagem = "Procuro dicas para tornar código escrito em Kotlin mais indiomático.",
-        curso = Curso(
+    init {
+        val cursoProgramacao = Curso(
             id = 1,
             nome = "Primeiros passos com Kotlin",
             categoria = "Programação"
-        ),
-        autor = Usuario(
-            id = 1L,
-            nome = "Sávio Raires",
-            email = "savio@email.com"
         )
-    )
+        val topico = Topico(
+            id = 1,
+            titulo = "Como tornar código Kotlin mais indiomático?",
+            mensagem = "Procuro dicas para tornar código escrito em Kotlin mais indiomático.",
+            curso = cursoProgramacao,
+            autor = Usuario(
+                id = 1L,
+                nome = "Sávio Raires",
+                email = "savio@email.com"
+            )
+        )
+        val topico2 = Topico(
+            id = 2,
+            titulo = "Alternativas ao Optinal com Kotlin",
+            mensagem = "Quais approaches podemos usar no kotlin em substituição ao Optional?",
+            curso = cursoProgramacao,
+            autor = Usuario(
+                id = 3L,
+                nome = "Leticia",
+                email = "leticia@email.com"
+            )
+        )
+        val topico3 = Topico(
+            id = 3,
+            titulo = "Como criar uma Monad em Kotlin?",
+            mensagem = "Procuro exemplos de código usando recurso de Monads em Kotlin.",
+            curso = cursoProgramacao,
+            autor = Usuario(
+                id = 3L,
+                nome = "João",
+                email = "joao@email.com"
+            )
+        )
+        topicos = listOf(topico, topico2, topico3)
+    }
 
-    fun getTopicos() : List<Topico> = listOf(topico)
+    fun getTopicos(): List<Topico> = topicos
 }
